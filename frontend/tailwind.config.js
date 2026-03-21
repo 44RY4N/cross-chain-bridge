99ba23e9d430c8a0da39eff6211112e6f35c821d
44RY4N <aaryan23b@iiitg.ac.in>
2026-03-21 13:22:16 +0530

    initial commit and set up for frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Nunito", "sans-serif"],
+      },
+      colors: {
+        babyPink: "#ffc6da",
+        cream: "#fff7ed",
+        pastelBlue: "#dbeafe",
+      },
+    },
+  },
+  plugins: [],
+};
